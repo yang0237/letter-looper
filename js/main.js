5a5012923d42a0dac152dad3c1aa25d3f277b819
Ye Yang <yang0237@algonquinlive.com>
2017-05-24 11:22:04 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+/*global console, prompt, document*/
+
+// Declare the variable
+var letter,
+	i,
+	k;
+
+// Printing a "triangle"
+while (!letter) {
+	letter = prompt("What is the letter?");
+}
+
+for (i = 0; i < 10; i += 1) {
+	for (k = 0; k <= i; k += 1) {
+	    document.write(letter);
+	}
+	    document.write("<br>");
+}
 
